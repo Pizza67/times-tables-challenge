15a24d29826ce86f1fdbd256edd7851cd5206c30
Massimo Messore <massimo.messore@gmail.com>
2024-08-27 21:41:11 +0200

    Fixed unit tests

--- app/src/test/java/it/mmessore/timestableschallenge/data/persistency/FakeAppPreferences.kt
+++ app/src/test/java/it/mmessore/timestableschallenge/data/persistency/FakeAppPreferences.kt
@@ -6,6 +6,7 @@ class FakeAppPreferences(
     override var maxTable: Int = 9,
     override var playSounds: Boolean = true,
     override var extendedMode: Boolean = false,
+    override var autoConfirm: Boolean = false,
     override var overwriteBestScores: Boolean = false,
     override var useTimeLeft: Boolean = true,
     override var themeStyle: AppPreferences.AppThemeStyle = AppPreferences.AppThemeStyle.LIGHT,
